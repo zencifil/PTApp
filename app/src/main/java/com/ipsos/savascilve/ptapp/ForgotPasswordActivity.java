671f21a9a9df27934684c6d3b9fd46c98140362d
Savas Cilve <savas.cilve@Ipsos.com>
2016-10-04 13:34:59 +0300

    Initial commit

---
+++ app/src/main/java/com/ipsos/savascilve/ptapp/ForgotPasswordActivity.java
@@ -0,0 +1,11 @@
+package com.ipsos.savascilve.ptapp;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by savas.cilve on 03.10.2016.
+ */
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+}
